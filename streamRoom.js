b055ab3b14a5803074abfbe8fc23999be96949c5
mike <mpobrien005@gmail.com>
2011-04-15 07:37:12 -0400

    more multiroom work

---
+++ streamRoom.js
@@ -0,0 +1,57 @@
+var mp3   = require('./mp3stream.js')
+var fs    = require('fs')
+
+var StreamRoom = function(roomName, redisClient){
+  var listeners = [];
+  var that = this;
+  var mp3Stream = new mp3.Mp3Stream();
+  var nowPlaying = null;
+
+  this.getNowPlaying = function(){
+    return nowPlaying;
+  }
+
+  var streamWriterFunc = function(data, y, z){
+    listeners.forEach(function(listener){
+      try{
+        listener.write(data);
+      }catch(e){}
+    });
+  }
+
+  mp3Stream.on("frameready", streamWriterFunc);
+  mp3Stream.on("stream-end", function(){
+    that.playNextFile()
+  });
+
+  this.addNewListener = function(req, listener){
+    listeners.push(listener);
+    req.connection.addListener("close", function(){ listeners.remove(listener); } );
+  }
+
+  this.playNextFile = function(endingFile){
+    redisClient.lpop("roomqueue_" + roomName, function(err, reply){ //TODO catch errors.
+      if(!reply){
+        nowPlaying = null;
+        if( endingFile ){
+          that.emit("file-end", roomName, endingFile);
+        }
+        console.log("queue was empty.");
+        return; // Nothing on the queue.
+      }
+      songInfo = JSON.parse(reply);  //TODO check for an error here!
+      nowPlaying = songInfo;
+      fs.readFile(songInfo.path, function(err, data){//TODO make this operate on chunked buffer/read, not entire file (less memory)
+        that.emit("file-change", roomName, endingFile, nowPlaying);
+        if(err) console.log("err:",err);
+        mp3Stream.streamFile(data);
+        console.log("playing: " + reply);
+      });
+    });
+  }
+
+};
+
+StreamRoom.prototype = new process.EventEmitter()
+
+exports.StreamRoom = StreamRoom;
