e356980f9864ab4690135989e6a8246c51a3b8f0
steven <steven@urethrafranklin.org>
2011-05-17 12:29:51 -0400

    new splash

---
+++ static/agile_carousel_data.php
@@ -0,0 +1,9 @@
+[{
+      "content": "<div class='slide_inner'><img class='photo' src='http://placekitten.com/510/330'></a></div>"
+}, {
+      "content": "<div class='slide_inner'><img class='photo' src='http://placekitten.com/510/329'></a></div>"
+}, {
+      "content": "<div class='slide_inner'><img class='photo' src='http://placekitten.com/510/332'></a></div>"
+}, {
+      "content": "<div class='slide_inner'><img class='photo' src='http://placekitten.com/510/333'></a></div>"
+}]
